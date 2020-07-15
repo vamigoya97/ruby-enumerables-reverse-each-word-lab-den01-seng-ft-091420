f2b5798b45483e22bbf0203c7955246fc7511202
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-15 19:24:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,15 @@ def reverse_each_word(string)
   return reversed.join(' ')
   
 end
+
+def reverse_each_word(string)
+  words = string.split(" ")
+  reversed = Array.new 
+  
+  words.collect do |word|
+    reversed << word.reverse
+  end
+  
+  return reversed.join(' ')
+  
+end
